5bfad8a7ca948bdc9279c81f52c498dfebca3fa9
sung <sung@wiremind.fr>
2019-11-20 15:01:04 +0100

    wip

--- src/main/kotlin/com/github/sylux6/maia/api/Maia.kt
+++ src/main/kotlin/com/github/sylux6/maia/api/Maia.kt
@@ -1,4 +1,22 @@
 package com.github.sylux6.maia.api
 
-class Maia {
+import com.github.kittinunf.fuel.core.FuelManager
+
+/**
+ * Class to make api requests
+ */
+class Maia() {
+
+    init {
+        FuelManager.instance.basePath = MaiaInfo.BASE_URL
+//        FuelManager.instance.baseHeaders = mapOf("Authorization" to option)
+    }
+
+    /**
+     * @since 1.0.0
+     *
+     * Get information about a beatmap by id
+     */
+    fun getBeatmapById() {
+    }
 }
---
+++ src/main/kotlin/com/github/sylux6/maia/api/MaiaInfo.kt
@@ -0,0 +1,7 @@
+package com.github.sylux6.maia.api
+
+object MaiaInfo {
+    const val VERSION = "1.0.0"
+    const val BASE_URL = ""
+    const val USER_AGENT = "Maia/v$VERSION (https://github.com/Sylux6/Maia)"
+}
